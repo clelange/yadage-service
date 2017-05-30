821a08ddd989279774e557eef88dafda463594a4
lukasheinrich <lukas.heinrich@gmail.com>
2017-05-30 17:49:48 +0200

    newline

--- yadageservice/app.py
+++ yadageservice/app.py
@@ -112,7 +112,7 @@ def monitor(identifier):
 @app.route('/subjob_monitor/<identifier>')
 @cern_oauth.login_required
 def subjob_monitor(identifier):
-    return ''.join(wflowapi.subjob_log(identifier))
+    return '\n'.join(wflowapi.subjob_log(identifier))
 
 @app.route('/jobstatus/<identifier>')
 @cern_oauth.login_required

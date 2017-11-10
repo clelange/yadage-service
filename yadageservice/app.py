a7c4be0441d2820477696a74e12c060b0bfa1f68
lukasheinrich <lukas.heinrich@gmail.com>
2017-11-11 00:00:56 +0100

    fix

--- yadageservice/app.py
+++ yadageservice/app.py
@@ -151,7 +151,7 @@ def subjob_logs(identifier):
     topic = request.args.get('topic', 'run')
     def generate():
         for msg in wflowapi.subjob_messages(identifier, topic = topic):
-            yield msg + '\n'
+            yield json.dumps(msg) + '\n'
     return Response(generate(), mimetype='text/plain')
 
 @app.route('/jobstatus/<identifier>')

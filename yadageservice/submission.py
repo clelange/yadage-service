c093a45fa1a71423976a6830edcf14f4eb3b4192
lukasheinrich <lukas.heinrich@gmail.com>
2017-12-01 14:34:17 +0100

    more html

--- yadageservice/submission.py
+++ yadageservice/submission.py
@@ -22,7 +22,7 @@ def submit_spec(request_json):
             'user': os.environ['YADAGE_SHIPTARGET_USER'],
             'port': os.environ['YADAGE_SHIPTARGET_PORT'],
         },
-        'meta_details': request_json['meta_details'],
+        'meta_details': request_json.get('meta_details',{}),
         'queue': os.environ['YADAGE_WORKFLOW_QUEUE'],
         'inputURL': request_json['inputURL'],
         'inputAuth': request_json.get('inputAuth',False),

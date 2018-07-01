1bddd44876e40853c67d3231b73378e1aeeac6b6
Clemens Lange <clemens.lange@cern.ch>
2018-07-01 11:41:11 +0200

    fix oauth

--- yadageservice/cern_oauth.py
+++ yadageservice/cern_oauth.py
@@ -5,7 +5,7 @@ import os
 import requests
 from flask import redirect, url_for, request
 from authlib.flask.client import OAuth
-from flask_login import login_user, logout_user
+from flask_login import login_required, login_user, logout_user
 import login as login_module
 
 def user_data(access_token):
@@ -39,20 +39,26 @@ def extract_user_info(userdata):
 
 oauth = OAuth()
 oauth_app = oauth.register('oauth_app',
-                             base_url=None,
-                             request_token_url=None,
-                             access_token_url=os.environ['CERN_OAUTH_TOKENURL'],
-                             authorize_url=os.environ[
-                                 'CERN_OAUTH_AUTHORIZEURL'],
-                             consumer_key=os.environ[
-                                 'CERN_OAUTH_APPID'],
-                             consumer_secret=os.environ[
-                                 'CERN_OAUTH_SECRET'],
-                             request_token_params={
-                                 'response_type': 'code', 'scope': 'bio'},
-                             access_token_params={
-                                 'grant_type': 'authorization_code'},
-                             access_token_method='POST'
+                           client_id=os.environ['CERN_OAUTH_APPID'],
+                           client_secret=os.environ['CERN_OAUTH_SECRET'],
+                           request_token_url=os.environ['CERN_OAUTH_TOKENURL'],
+                           access_token_url=os.environ['CERN_OAUTH_TOKENURL'],
+                           authorize_url=os.environ['CERN_OAUTH_AUTHORIZEURL'],
+                           api_base_url='https://oauthresource.web.cern.ch/api/',
+                            #  base_url=None,
+                            #  request_token_url=None,
+                            #  access_token_url=os.environ['CERN_OAUTH_TOKENURL'],
+                            #  authorize_url=os.environ[
+                            #      'CERN_OAUTH_AUTHORIZEURL'],
+                            #  consumer_key=os.environ[
+                            #      'CERN_OAUTH_APPID'],
+                            #  consumer_secret=os.environ[
+                            #      'CERN_OAUTH_SECRET'],
+                            #  request_token_params={
+                            #      'response_type': 'code', 'scope': 'bio'},
+                            #  access_token_params={
+                            #      'grant_type': 'authorization_code'},
+                            #  access_token_method='POST'
                              )
 
 def oauth_redirect(resp):
@@ -87,20 +93,21 @@ def login():
         return redirect(next_url)
 
     redirect_uri = os.environ['CERN_OAUTH_BASEURL'] + url_for('oauth_redirect')
-    return oauth_app.authorize(callback=redirect_uri)
+    return oauth_app.authorize_redirect(redirect_uri)
 
 def logout():
     logout_user()
     return redirect('/')
 
 def init_app(app):
+    oauth.init_app(app)
     login_module.login_manager.init_app(app)
     login_module.login_manager.login_view = 'login'
 
     global oauth_redirect
     global login
     global logout
-    oauth_redirect = oauth_app.authorized_handler(oauth_redirect)
+    # oauth_redirect = oauth_app.authorize_access_token
     oauth_redirect = app.route(os.environ['CERN_OAUTH_REDIRECT_ROUTE'])(oauth_redirect)
     login  = app.route('/login')(login)
     logout = app.route('/logout')(logout)
